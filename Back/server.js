85113541f5ac20b0b8a194c9552a27e89158cc26
MerloMarianela <marianela@Merlo>
2021-03-10 15:47:05 -0300

    prueba

--- Back/server.js
+++ Back/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const db = require("./db")
 const Routes = require("../Back/routes")
+const bodyParser = require("body-parser")
 const PORT = 3001;
 
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
 app.use("/api", Routes)
 
 db.sync().then(() => {
