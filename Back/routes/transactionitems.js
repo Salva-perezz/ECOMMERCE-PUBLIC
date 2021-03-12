b747be21ff8392ab6c1a06fa08d199afec76aada
MarianoSou <mpsoule@fibertel.com.ar>
2021-03-12 17:45:00 -0300

    ultimos cambios routes

--- Back/routes/transactionitems.js
+++ Back/routes/transactionitems.js
@@ -13,58 +13,83 @@ router.post("/", (req, res) => {
     })
 })
 
+// router.post("/", (req, res) => {
+//     TransactionItem.findOne({
+//         where: {
+//             transactionId: req.body.transactionId,
+//             productId: req.body.productId
+//         }
+//     }).then(transactionItem => {
+//         if (!transactionItem) {
+//             TransactionItem.create(req.body)
+//                 .then(transactionItem => {
+//                     transactionItem.setTransaction(req.body.transactionId),
+//                         transactionItem.setProduct(req.body.productId)
+//                 })
+//                 .then(transItem => res.send(transItem))
+
+//         } else {
+//             transactionItem.update({ quantity: req.body.quantity + transactionItem.quantity })
+//                 .then(transactionItem => res.send(transactionItem))
+//         }
+//     }).catch((error) => {
+//         console.log(error)
+//         res.sendStatus(500)
+//     })
+// })
+
 router.put("/remove", (req, res) => {
-  console.log("REQ BODY DEL DELETE", req.body)
-  TransactionItem.destroy({
-    where: { id: req.body.id },
-  })
-    .then(() => {
-      res.status(200).send("Item Eliminado")
+    console.log("REQ BODY DEL DELETE", req.body)
+    TransactionItem.destroy({
+        where: { id: req.body.id },
     })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+        .then(() => {
+            res.status(200).send("Item Eliminado")
+        })
+        .catch(() => {
+            res.sendStatus(500)
+        })
 })
 
 router.put("/", (req, res) => {
-  transactionItem
-    .update(
-      { quantity: req.body.quantity },
-      {
-        where: { id: req.body.id },
-        returning: true,
-      }
-    )
-    .then((transactionItemUpdated) => {
-      transactionItemUpdated = transactionItemUpdated[1]
-      res.status(200).json(transactionItemUpdated)
-    })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+    transactionItem
+        .update(
+            { quantity: req.body.quantity },
+            {
+                where: { id: req.body.id },
+                returning: true,
+            }
+        )
+        .then((transactionItemUpdated) => {
+            transactionItemUpdated = transactionItemUpdated[1]
+            res.status(200).json(transactionItemUpdated)
+        })
+        .catch(() => {
+            res.sendStatus(500)
+        })
 })
 
 router.put("/load", (req, res) => {
-  TransactionItem.findAll({
-    where: { transactionId: req.body.transactionId },
-    include: Product,
-  })
-    .then((transactionItems) => {
-      transactionItems = transactionItems.map((item) => {
-        return {
-          id: item.id,
-          productId: item.productId,
-          quantity: item.quantity,
-          name: item.product.name,
-          price: item.product.price,
-          urlPicture: item.product.urlPicture
-        }
-      })
-      res.status(200).json(transactionItems)
+    TransactionItem.findAll({
+        where: { transactionId: req.body.transactionId },
+        include: Product,
     })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+        .then((transactionItems) => {
+            transactionItems = transactionItems.map((item) => {
+                return {
+                    id: item.id,
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    name: item.product.name,
+                    price: item.product.price,
+                    urlPicture: item.product.urlPicture
+                }
+            })
+            res.status(200).json(transactionItems)
+        })
+        .catch(() => {
+            res.sendStatus(500)
+        })
 })
 
 module.exports = router
