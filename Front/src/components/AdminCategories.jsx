904e5a1cbbdcf534c542e4abdf98427db81ccbc9
maxsznaider <maxsznaider@yahoo.com>
2021-03-15 12:38:42 -0300

    Create Admin Components

---
+++ Front/src/components/AdminCategories.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const AdminCategories = () => <div></div>
+
+export default AdminCategories
---
+++ Front/src/components/AdminProduct.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const AdminUsers = () => <div></div>
+
+export default AdminUsers
