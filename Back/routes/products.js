e45d26355a4cb87ab1c9f4ef2573f2f3c94df385
MarianoSou <mpsoule@fibertel.com.ar>
2021-03-15 12:38:34 -0300

    nuevas rutas

--- Back/routes/products.js
+++ Back/routes/products.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
+const { Op } = require("sequelize")
 const { Product } = require("../models");
 
 
@@ -6,65 +7,93 @@ router.post("/", (req, res) => {
     Product.create(req.body)
         .then((product) => {
             res.status(201).json(product)
-        })
-        .catch((err) => {
+        }).catch((err) => {
             console.log(err)
-            res.sendStatus()
+            res.sendStatus(400)
         })
+})
+
+
+router.get("/search", (req, res) => {
+    if (req.query.c) { //Category
+        Product.findAll({
+            where: {
+                [req.query.c]: { [Op.like]: `%${req.query.s}%` } //Category y Search
+            }, offset: req.query.o, //skip n resultados
+            limit: 12 // n resultados x fetch
+        }).then((products) => {
+            res.status(200).json(products)
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+    }
+    else {
+        const search = req.query.s.split(" ").map((ob) => { return `%${ob.charAt(0).toUpperCase() + ob.slice(1)}%` }) //Mayuscula a la primera letra de cada item
+        Product.findAll({
+            where: {
+                [Op.or]: [
+                    { name: { [Op.like]: { [Op.any]: search } } }, //arreglo de datos de busqueda
+                    { brand: { [Op.like]: { [Op.any]: search } } }, // arreglo de datos de busqueda
+                ],
+            }, offset: req.query.o, //skip n resultados
+            limit: 12 //n resultados x fetch
+        }).then((products) => {
+            res.status(200).json(products)
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+    }
+})
+
+router.get("/", (req, res) => {
+    Product.findAll({
+        offset: req.query.l, //desde que n de registro muestro
+        limit: 12
+    }).then((products) => {
+        res.status(200).json(products)
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
 })
 
 router.get("/:id", (req, res) => {
     Product.findByPk(req.params.id)
         .then((product) => {
             res.status(200).json(product)
-        }).catch((error) => {
-            console.log(error)
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
         })
 })
-
-// router.get("/search", (req, res) => {
-//     Product.findAll({
-//         where: req.body
-//     }).then((product) => {
-//         res.status(200).json(product)
-//     }).catch(() => {
-//         res.sendStatus(500)
-//     })
-// })
-
-
-router.get("/", (req, res) => {
-    Product.findAll()
-        .then((products) => {
-            res.status(200).json(products)
-        }).catch((error) => {
-            console.log(error)
-            res.sendStatus(500)
-        })
-})
-
 
 router.put("/:id", (req, res) => {
     Product.update(req.body, {
         where: { id: req.params.id },
         returning: true,
+        plain: true
     }).then((product) => {
-        product = product[1]
-        res.status(200).json(product)
-    }).catch(() => {
-        res.sendStatus(500)
+        res.status(200).json(product[1])
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
     })
 })
-
 
 router.delete("/:id", (req, res) => {
-    Product.delete({
+    Product.destroy({
         where: { id: req.params.id }
     }).then(() => {
-        res.status(200).send("Product Deleted")
-    }).catch(() => {
-        res.sendStatus(500)
+        res.status(200).json()
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
     })
 })
+
+
+
 
 module.exports = router;
--- Back/routes/transactionitems.js
+++ Back/routes/transactionitems.js
@@ -1,127 +1,69 @@
 const router = require("express").Router()
 const { TransactionItem, Transaction, Product } = require("../models")
 
-// router.post("/", (req, res) => {
-//   TransactionItem.create(req.body)
-//     .then((transactionItem) => {
-//       transactionItem.setTransaction(req.body.transactionId)
-//       transactionItem.setProduct(req.body.productId)
-//       res.status(201).json(transactionItem)
-//     })
-//     .catch((error) => {
-//      console.log(error)
-//       res.sendStatus(404)
-//     })
-// })
-
 router.post("/", (req, res) => {
-<<<<<<< HEAD
     TransactionItem.findOne({
         where: {
             transactionId: req.body.transactionId,
-            productId: req.body.productId
-        }
-    }).then(transactionItem => {
-        if (!transactionItem) {
-            TransactionItem.create(req.body)
-                .then(transactionItem => {
-                    transactionItem.setTransaction(req.body.transactionId),
-                        transactionItem.setProduct(req.body.productId)
-                })
-                .then(transItem => res.send(transItem))
-
-        } else {
-            transactionItem.update({ quantity: req.body.quantity + transactionItem.quantity })
-                .then(transactionItem => res.send(transactionItem))
-        }
-    }).catch((error) => {
-        console.log(error)
-        res.sendStatus(404)
-=======
-  TransactionItem.findOne({
-    where: {
-      transactionId: req.body.transactionId,
-      productId: req.body.productId,
-    },
-  })
-    .then((transactionItem) => {
-      if (!transactionItem) {
-        TransactionItem.create(req.body)
-          .then((transactionItem) => {
-            transactionItem.setTransaction(req.body.transactionId)
-            transactionItem.setProduct(req.body.productId)
-            return transactionItem
-          })
-          .then((transItem) => res.send(transItem))
-      } else {
-        transactionItem
-          .update({
-            quantity:
-              Number(req.body.quantity) + Number(transactionItem.quantity),
-          })
-          .then((transactionItem) => res.send(transactionItem))
-      }
+            productId: req.body.productId,
+        },
     })
-    .catch((error) => {
-      console.log(error)
-      res.sendStatus(500)
->>>>>>> 5d97ed885198dd527ddff52e95608cd52a028b9c
-    })
+        .then((transactionItem) => {
+            if (!transactionItem) {
+                TransactionItem.create(req.body)
+                    .then(transactionItem => transactionItem.setTransaction(req.body.transactionId))
+                    .then(transactionItem => transactionItem.setProduct(req.body.productId))
+                    .then((transactionItem) => res.send(transactionItem))
+            } else {
+                transactionItem
+                    .update({
+                        quantity:
+                            Number(req.body.quantity) + Number(transactionItem.quantity),
+                    })
+                    .then((transactionItem) => res.send(transactionItem))
+            }
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
+//Modificar por delete con req.params.id
 router.put("/remove", (req, res) => {
-  TransactionItem.destroy({
-    where: { id: req.body.id },
-  })
-    .then(() => {
-      res.status(200).send("Item Eliminado")
+    TransactionItem.destroy({
+        where: { id: req.body.id },
     })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+        .then(() => {
+            res.status(200).send()
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
 router.put("/", (req, res) => {
-  transactionItem
-    .update(
-      { quantity: req.body.quantity },
-      {
-        where: { id: req.body.id },
-        returning: true,
-      }
-    )
-    .then((transactionItemUpdated) => {
-      transactionItemUpdated = transactionItemUpdated[1]
-      res.status(200).json(transactionItemUpdated)
-    })
-    .catch(() => {
-      res.sendStatus(500)
-    })
+    transactionItem
+        .update(
+            { quantity: req.body.quantity },
+            {
+                where: { id: req.body.id },
+                returning: true,
+                plain: true
+            }
+        )
+        .then((transactionItemUpdated) => {
+            res.status(200).json(transactionItemUpdated[1])
+        }).catch((err) => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
 router.put("/load", (req, res) => {
-  TransactionItem.findAll({
-    where: { transactionId: req.body.transactionId },
-    include: Product,
-  })
-    .then((transactionItems) => {
-      transactionItems = transactionItems.map((item) => {
-        return {
-          id: item.id,
-          productId: item.productId,
-          quantity: item.quantity,
-          name: item.product.name,
-          price: item.product.price,
-          urlPicture: item.product.urlPicture,
-        }
-      })
-      res.status(200).json(transactionItems)
+    TransactionItem.findAll({
+        where: { transactionId: req.body.transactionId },
+        include: Product,
     })
-    .catch((err) => {
-      console.log(err)
-      res.sendStatus(500)
-    })
-<<<<<<< HEAD
         .then((transactionItems) => {
             transactionItems = transactionItems.map((item) => {
                 return {
@@ -130,17 +72,27 @@ router.put("/load", (req, res) => {
                     quantity: item.quantity,
                     name: item.product.name,
                     price: item.product.price,
-                    urlPicture: item.product.urlPicture
+                    urlPicture: item.product.urlPicture,
                 }
             })
             res.status(200).json(transactionItems)
+            res.sendStatus(400)
+
         })
-        .catch((error) => {
-            console.log(error)
-            res.sendStatus(404)
-        })
-=======
->>>>>>> 5d97ed885198dd527ddff52e95608cd52a028b9c
+})
+
+router.get("/:id", (req, res) => {
+    TransactionItem.findAll({
+        include: [{
+            model: Transaction,
+            where: { userId: req.params.id }
+        }, Product],
+    }).then((transactionItems) => {
+        res.status(201).json(transactionItems)
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
 })
 
 module.exports = router
