0965328fe3134195a00d119ba008a35c40c6ae26
MarianoSou <mpsoule@fibertel.com.ar>
2021-03-17 15:02:29 -0300

    envio de mail listo

--- Back/routes/categories.js
+++ Back/routes/categories.js
@@ -32,7 +32,7 @@ router.put('/types/:id', (req, res) => {
 
 //YEARS
 
-router.get('/years/', (req, res) => {
+router.get('/years', (req, res) => {
    Year.findAll()
       .then(years => res.status(200).json(years))
 });
